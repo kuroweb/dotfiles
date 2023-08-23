1f5cc8f1eb5a13cd1ae7a2baa8cd740264eb7c97
kuroweb <kuromitsu0104@gmail.com>
2023-08-23 21:49:08 +0900

    feat: zshを調整

--- .config/zsh/prompt.zsh
+++ .config/zsh/prompt.zsh
@@ -1,5 +1,9 @@
 #!/bin/zsh
 
+#
+# prompt
+#
+
 autoload -Uz vcs_info
 setopt prompt_subst
 zstyle ':vcs_info:git:*' check-for-changes true
@@ -8,7 +12,6 @@ zstyle ':vcs_info:git:*' unstagedstr "%F{cyan}"
 zstyle ':vcs_info:*' formats "%F{cyan}%c%u[%b]%f"
 zstyle ':vcs_info:*' actionformats '[%b|%a]'
 precmd () { vcs_info }
-
 PROMPT='
 [%B%F{magenta}%n@%m%f%b:%F{green}%(5~,%-1~/.../%2~,%~)%f]%F{cyan}$vcs_info_msg_0_%f
 %F{magenta}%B$%b%f '
