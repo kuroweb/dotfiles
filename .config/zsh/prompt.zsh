fc7c227c7eef831701cbc949f507abd874becef4
kuroweb <kuromitsu0104@gmail.com>
2023-09-06 14:24:19 +0900

    feat: 不要なコードを削除する

--- .config/zsh/prompt.zsh
+++ .config/zsh/prompt.zsh
@@ -4,20 +4,6 @@
 # prompt
 #
 
-# autoload -Uz vcs_info
-# setopt prompt_subst
-# zstyle ':vcs_info:git:*' check-for-changes true
-# zstyle ':vcs_info:git:*' stagedstr "%F{magenta}"
-# zstyle ':vcs_info:git:*' unstagedstr "%F{cyan}"
-# zstyle ':vcs_info:*' formats "%F{cyan}%c%u[%b]%f"
-# zstyle ':vcs_info:*' actionformats '[%b|%a]'
-# precmd () { vcs_info }
-# PROMPT='
-# [%B%F{magenta}%n@%m%f%b:%F{green}%(5~,%-1~/*/%2~,%~)%f]%F{cyan}$vcs_info_msg_0_%f
-# %F{magenta}%B$%b%f '
-
-precmd () { powerline_precmd }
-
 function powerline_precmd() {
     PS1="$(powerline-shell --shell zsh)"$'\n'"$ "
 }
@@ -34,3 +20,5 @@ function install_powerline_precmd() {
 if [ "$TERM" != "linux" -a -x "$(command -v powerline-shell)" ]; then
     install_powerline_precmd
 fi
+
+precmd() { powerline_precmd }
