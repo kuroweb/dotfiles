0364843f89797912aad06c7d2dbb4a1ca3c3c51a
kuroweb <kuromitsu0104@gmail.com>
2023-12-09 11:34:38 +0900

    feat: vscodeの設定ファイルを管理

--- .config/vscode/install.sh
+++ .config/vscode/install.sh
@@ -1,19 +1,17 @@
 #!/bin/sh
-SCRIPT_DIR=$(cd $(dirname $0) && pwd)
+SCRIPT_DIR=$(pwd)
 VSCODE_SETTING_DIR=~/Library/Application\ Support/Code/User
 
 # backup
-mkdir "$SCRIPT_DIR/backup"
 cp "$VSCODE_SETTING_DIR/settings.json" "$SCRIPT_DIR/backup"
 cp "$VSCODE_SETTING_DIR/keybindings.json" "$SCRIPT_DIR/backup"
 code --list-extensions > "$SCRIPT_DIR/backup/extensions"
 
+rm "$VSCODE_SETTING_DIR/settings.json"
+ln -s "$SCRIPT_DIR/settings.json" "${VSCODE_SETTING_DIR}/settings.json"
 
-# rm "$VSCODE_SETTING_DIR/settings.json"
-# ln -s "$SCRIPT_DIR/settings.json" "${VSCODE_SETTING_DIR}/settings.json"
-
-# rm "$VSCODE_SETTING_DIR/keybindings.json"
-# ln -s "$SCRIPT_DIR/keybindings.json" "${VSCODE_SETTING_DIR}/keybindings.json"
+rm "$VSCODE_SETTING_DIR/keybindings.json"
+ln -s "$SCRIPT_DIR/keybindings.json" "${VSCODE_SETTING_DIR}/keybindings.json"
 
 cat ./extensions | while read line
 do
