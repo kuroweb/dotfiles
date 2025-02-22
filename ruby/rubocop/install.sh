6d3b26d8941eb691fcb565c2a2c324333f7e6e30
kuroweb <kuromitsu0104@gmail.com>
2025-02-23 02:19:13 +0900

    feat: update

--- ruby/rubocop/install.sh
+++ ruby/rubocop/install.sh
@@ -5,7 +5,10 @@ BASE_PATH=$(dirname $0)
 cd $BASE_PATH
 source .env
 
-rbenv local $RUBY_VERSION
+export RBENV_VERSION=$RUBY_VERSION
+eval "$(rbenv init -)"
+
 bundle config --local path vendor/bundler
 bundle config --local bin vendor/bin
+# bundle package
 bundle install --local
