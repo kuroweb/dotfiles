d86a9e38a7e102c8c07d0d89351445003c434bbf
kuroweb <kuromitsu0104@gmail.com>
2026-02-14 00:41:17 +0900

    feat: 微調整

--- nvim/lua/plugins/barbar.lua
+++ nvim/lua/plugins/barbar.lua
@@ -1,19 +1,35 @@
 -- バッファをタブのように表示する (barbar.nvim)
 return {
-  "romgrk/barbar.nvim",
-  dependencies = {
-    "lewis6991/gitsigns.nvim",
-    "nvim-tree/nvim-web-devicons",
-  },
-  opts = {
-    icons = {
-      buffer_index = false,
-      buffer_number = false,
-      gitsigns = {
-        added = { enabled = true, icon = "+" },
-        changed = { enabled = true, icon = "~" },
-        deleted = { enabled = true, icon = "-" },
+  {
+    "romgrk/barbar.nvim",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    init = function()
+      vim.g.barbar_auto_setup = false
+    end,
+    keys = {
+      { "<Leader>bp", "<Cmd>BufferPrevious<CR>", desc = "Previous buffer" },
+      { "<Leader>bn", "<Cmd>BufferNext<CR>", desc = "Next buffer" },
+      { "<Leader>bc", "<Cmd>BufferClose<CR>", desc = "Close buffer" },
+      { "<Leader>bo", "<Cmd>BufferCloseAllExceptCurrent<CR>", desc = "Close other buffers" },
+      { "<Leader>bq", "<Cmd>BufferPick<CR>", desc = "Pick buffer (jump by letter)" },
+      { "<Leader>b1", "<Cmd>BufferGoto 1<CR>", desc = "Goto buffer 1" },
+      { "<Leader>b2", "<Cmd>BufferGoto 2<CR>", desc = "Goto buffer 2" },
+      { "<Leader>b3", "<Cmd>BufferGoto 3<CR>", desc = "Goto buffer 3" },
+      { "<Leader>b4", "<Cmd>BufferGoto 4<CR>", desc = "Goto buffer 4" },
+      { "<Leader>b5", "<Cmd>BufferGoto 5<CR>", desc = "Goto buffer 5" },
+    },
+    opts = {
+      animation = true,
+      clickable = true,
+      icons = {
+        filetype = { enabled = true },
+      },
+      sidebar_filetypes = {
+        NvimTree = true,
       },
     },
+    config = function(_, opts)
+      require("barbar").setup(opts)
+    end,
   },
 }
